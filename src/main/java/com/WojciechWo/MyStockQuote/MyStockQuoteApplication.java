c1e296bc5c4d3f84318305782b969e64f459f77e
Wołoszyn Wojciech <wojtek201990@gmail.com>
2022-08-21 09:58:56 +0200

    Initial commit

---
+++ src/main/java/com/WojciechWo/MyStockQuote/MyStockQuoteApplication.java
@@ -0,0 +1,13 @@
+package com.WojciechWo.MyStockQuote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyStockQuoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyStockQuoteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/WojciechWo/MyStockQuote/MyStockQuoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.WojciechWo.MyStockQuote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyStockQuoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
